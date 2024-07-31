ce8b1b625623031cb1f0f92dcaa95714a894f365
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-31 17:19:30 -0500

    Comment-out `encfs` usage

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -8,18 +8,19 @@ if [ -z "$ENCFS_PASSWORD" ]; then
   exit 1
 fi
 
-# Initialize or mount the encrypted filesystem as the mosquitto user
-if [ ! -f /encrypted/.encfs6.xml ]; then
-  echo "Initializing encrypted filesystem"
-  su mosquitto -c "echo \"$ENCFS_PASSWORD\" | encfs --standard --stdinpass /encrypted /var/lib/mosquitto --verbose"
-else
-  echo "Mounting encrypted filesystem"
-  su mosquitto -c "echo \"$ENCFS_PASSWORD\" | encfs --stdinpass /encrypted /var/lib/mosquitto --verbose"
-fi
+# TODO: Uncomment
+# # Initialize or mount the encrypted filesystem as the mosquitto user
+# if [ ! -f /encrypted/.encfs6.xml ]; then
+#   echo "Initializing encrypted filesystem"
+#   su mosquitto -c "echo \"$ENCFS_PASSWORD\" | encfs --standard --stdinpass /encrypted /var/lib/mosquitto --verbose"
+# else
+#   echo "Mounting encrypted filesystem"
+#   su mosquitto -c "echo \"$ENCFS_PASSWORD\" | encfs --stdinpass /encrypted /var/lib/mosquitto --verbose"
+# fi
 
-# # Debug: Check if encfs is mounted
-echo "Checking if encfs is mounted:"
-mount | grep encfs || echo "encfs is not mounted"
+# # # Debug: Check if encfs is mounted
+# echo "Checking if encfs is mounted:"
+# mount | grep encfs || echo "encfs is not mounted"
 
 # Run Mosquitto
 exec mosquitto -c /mosquitto/config/mosquitto.conf
