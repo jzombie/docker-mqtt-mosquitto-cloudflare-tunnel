ef13877ebc80e2b5781255bb1c1e168b3fac5868
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-30 22:13:12 -0500

    Auto-save on changes and persist data to `data` directory

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
 # Set permissions for the aclfile
 chmod 0700 /mosquitto/config/aclfile
+
+# Ensure the persistence directory exists and set appropriate permissions
+mkdir -p /var/lib/mosquitto
+chown mosquitto:mosquitto /var/lib/mosquitto
+chmod 0755 /var/lib/mosquitto
 
 # Start the Mosquitto service
 exec mosquitto -c /mosquitto/config/mosquitto.conf
