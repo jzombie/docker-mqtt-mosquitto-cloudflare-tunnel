4c803937a09525b9608ba519be4eca9944eb51f4
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-07-31 23:43:23 -0500

    Debug

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -64,11 +64,8 @@ fi
 echo "Contents of /var/lib/mosquitto after checking for persistence file:"
 ls -l /var/lib/mosquitto
 
-# Set correct permissions for aclfile
-echo "Setting permissions for aclfile"
-ls -l /mosquitto/config/aclfile  # Debug: Check current permissions
 chmod 0700 /mosquitto/config/aclfile
-ls -l /mosquitto/config/aclfile  # Debug: Verify permissions after change
+chown mosquitto:mosquitto /mosquitto/config/aclfile
 
 # Run Mosquitto as the mosquitto user
 exec su mosquitto -c "mosquitto -c /mosquitto/config/mosquitto.conf"
